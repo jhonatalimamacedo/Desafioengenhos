02ae95df3ac9054a6707486d050c9d4faf05c2f8
jhonata <jhonata.limamacedoti@gmail.com>
2022-07-12 03:50:16 -0300

    desafio

---
+++ desafio1/desafio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um valor");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            else if(i%5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
---
+++ desafio2/desafio2/desafio2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio2
+{
+    public partial class Form1 : Form
+    {
+        private int _ticks = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "Desligado" && _ticks != 0)
+            {
+                label1.Text = "Ligado";
+                timer1.Start();
+            }
+            else
+            {
+                label1.Text = "Desligado";
+                timer1.Stop();
+            };
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Text == "Ligado" && _ticks != 0)
+            {
+                _ticks--;
+                label2.Text = _ticks.ToString();
+            }
+            else
+            {
+                label1.Text = "Desligado";
+                timer1.Stop();
+            }
+        }
+    }
+}
